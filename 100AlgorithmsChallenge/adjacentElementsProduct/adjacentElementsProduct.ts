161cfdf32118d31d226310dc79ad819abb1a9cb5
AlainDlcTVOP <alain_dlc@outlook.com>
2021-02-16 14:55:05 +0100

    allLongString Challange done

--- 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100AlgorithmsChallenge/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -2,21 +2,21 @@ export function adjacentElementsProduct(inputArray: number[]): number {
 
     let sumOfProduct = inputArray[0] * inputArray[1];
 
-
-
     for (let i = 1; i < inputArray.length - 1; i++) {
-
         let product = inputArray[i] * inputArray[i + 1];
 
         console.log(product);
-        console.log(sumOfProduct);
 
-        sumOfProduct = sumOfProduct < product ? product : product;
+        sumOfProduct = product ? product : product;
+        console.log(sumOfProduct);
     }
 
     return sumOfProduct;
 
-
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+/*For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+7 and 3 produce the largest product.*/
--- 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
+++ 100AlgorithmsChallenge/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
-export function allLongestStrings(inputArray: string[]): string[] {
+export function allLongestStrings(inputArray: string[]): any[] {
+
+    let longestString = 0;
+    const wordLength = [];
+
+
+
 
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+
+/*Given an array of strings, return another array containing all of its longest strings.
+
+** Example **
+
+    For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+allLongestStrings(inputArray) = ["aba", "vcd", "aba"]. */
