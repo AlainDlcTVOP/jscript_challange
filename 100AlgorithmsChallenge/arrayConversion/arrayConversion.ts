d4b97f2a535b25b3f5d04f646c77e031def462be
AlainDlcTVOP <alain_dlc@outlook.com>
2021-03-18 11:48:29 +0100

    arrayMax

--- 100AlgorithmsChallenge/arrayConversion/arrayConversion.ts
+++ 100AlgorithmsChallenge/arrayConversion/arrayConversion.ts
@@ -7,26 +7,22 @@ export function arrayConversion(inputArray: number[]): number {
         isOdd = !isOdd;
     }
 
-    console.log(inputArray);
     return inputArray[0];
 
 }
+
 function sumProduct(nums: number[], isOdd: boolean): number[] {
-    const sumProducts: number[] = [];
+    const sumProduct: number[] = [];
+
     if (isOdd) {
         for (let i = 0; i < nums.length; i += 2) {
-            sumProducts.push(nums[i] + nums[i + 1]);
+            sumProduct.push(nums[i] + nums[i + 1]);
         }
     } else {
         for (let i = 0; i < nums.length; i += 2) {
-            sumProducts.push(nums[i] * nums[i + 1]);
+            sumProduct.push(nums[i] * nums[i + 1]);
         }
     }
-    return sumProducts;
-
+    return sumProduct;
 }
-
-
-
-
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
--- 100AlgorithmsChallenge/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ 100AlgorithmsChallenge/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,35 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
 
-}
+    let sum = 0;
+    let max = 0;
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+    for (let i = 0; i < k; i++) {
+        sum += inputArray[i];
+    }
+    console.log(inputArray.length);
+    console.log(k)
+
+    max = sum;
+
+    for (let i = k; i < inputArray.length; i++) {
+        sum -= inputArray[i - k];
+        sum += inputArray[i];
+        if (sum > max) {
+            max = sum;
+        }
+    }
+
+    return max;
+
+
+}
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+/*For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+arrayMaxConsecutiveSum(inputArray, k) = 8.
+All possible sums of 2 consecutive elements are:
+-   2 + 3 = 5;
+-   3 + 5 = 8;
+-   5 + 1 = 6;
+-   1 + 6 = 7.
+
+   Thus, the answer is 8 */
