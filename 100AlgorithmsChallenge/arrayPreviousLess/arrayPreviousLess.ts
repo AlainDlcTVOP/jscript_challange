216ccf5b3eb538cf24cac2d8a1be3aac0253fc91
AlainDlcTVOP <alain_dlc@outlook.com>
2021-03-29 14:06:26 +0200

    boxBlur

--- 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
+++ 100AlgorithmsChallenge/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,24 @@
 export function arrayPreviousLess(items: number[]): number[] {
 
-}
+    const lessThanList: number[] = [];
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+    for (let i = items.length - 1; i >= 0; i--) {
+        console.log(i);
+        for (let j = i; j >= 0; j--) {
+            console.log(j);
+            if (items[i] > items[j]) {
+                lessThanList.unshift(items[j]);
+                console.log(lessThanList);
+                break;
+            } else if (j === 0) {
+                lessThanList.unshift(-1);
+                console.log(lessThanList);
+            }
+        }
+    }
+
+    return lessThanList;
+
+}
+//output [-1, 3, -1, 2, 4]
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
--- 100AlgorithmsChallenge/avoidObstacles/avoidObstacles.ts
+++ 100AlgorithmsChallenge/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,17 @@
 export function avoidObstacles(inputArray: number[]): number {
 
+    inputArray = inputArray.sort((a, b) => a - b);
+    const largestArray = inputArray[inputArray.length - 1];
+
+    for (let i = 1; i < inputArray.length + 1; i++) {
+        if (inputArray.every((element) => element % i !== 0)) {
+            return i;
+        }
+    }
+
+
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+
+
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
