ab126962a32144b9b24f9eca0b7e4d84c3d6ceb7
AlainDlcTVOP <alain_dlc@outlook.com>
2021-04-05 13:30:52 +0200

    charChallange

--- 100AlgorithmsChallenge/chunkyMonkey/chunkyMonkey.ts
+++ 100AlgorithmsChallenge/chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,15 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
 
+    const num = [];
+    let count = 0;
+
+    while (count < arr.length) {
+        num.push(arr.slice(count, count += size));
+    }
+
+    return num;
+
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
--- 100AlgorithmsChallenge/circleOfNumbers/circleOfNumbers.ts
+++ 100AlgorithmsChallenge/circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,17 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
+    const circlenum = [];
+    const halfNum = n / 2;
+
+    for (let i = 0; i < n; i++) {
+        circlenum.push(i);
+    }
+
+    if (firstNumber < halfNum) {
+        return circlenum[firstNumber + halfNum];
+    }
+
+    return circlenum[firstNumber - halfNum];
 
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
